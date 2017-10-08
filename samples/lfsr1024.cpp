17491d9819e8545e02d917e8fee887c3911404cb
nmrr <nicolasmaurer1992@gmail.com>
2017-10-08 14:35:53 +0200

    Create lfsr1024.cpp

---
+++ samples/lfsr1024.cpp
@@ -0,0 +1,47 @@
+/*
+MIT License
+
+Copyright (c) 2017 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <iostream>
+#include "LFSR.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    // Create a 1024 bits register, by default all bits are set to 0
+    LFSR lfsr(1024);
+    
+    // Set the first bit to 1
+    lfsr.setBit(0, true);
+    
+    // X0 xor X2 xor X3
+    while(1)
+    {
+        for (int i=0;i<8;i++) lfsr.rightShift(lfsr.getFirstBit() xor lfsr.getBit(2) xor lfsr.getBit(3));
+        cout << lfsr.get8bit();
+    }
+
+    
+    return (0);
+}
